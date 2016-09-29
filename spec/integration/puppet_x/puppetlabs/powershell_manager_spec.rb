ca04e817036e577509ca55b97ae4c043af6071d7
Ethan J. Brown <Iristyle@github>
2016-09-28 21:55:08 -0700

    (maint) Run 64k write tests on all PS versions
     - Prior to PowerShell 3, the behavior of the Write-Output cmdlet
       introduces newlines automatically based on the current console
       width.  This behavior is not controllable in the host - as the
       custom PSHostUserInterface implementation will see multiple calls
       to WriteLine instead of just a single call to WriteLine.
    
       Since the captured output of stdout is used only for logging
       purposes within Puppet, modify the test to instead use Write-Host
       on PS2 and earlier versions.  This does not introduce extraneous
       newlines into the original string value.
    
       Note that the line ending is also different when using Write-Host
       rather than Write-Output.

--- spec/integration/puppet_x/puppetlabs/powershell_manager_spec.rb
+++ spec/integration/puppet_x/puppetlabs/powershell_manager_spec.rb
@@ -393,31 +393,42 @@ try {
       version
     end
 
-    it "should be able to write more than the 64k default buffer size to the managers pipe without deadlocking the Ruby parent process or breaking the pipe" do
-      pending("Powershell version less than 3.0 has different Write-Output behavior") if current_powershell_major_version < 3
+    def output_cmdlet
+      # Write-Output is the default behavior, except on older PS2 where the
+      # behavior of Write-Output introduces newlines after every width number
+      # of characters as specified in the BufferSize of the custom console UI
+      # Write-Host should usually be avoided, but works for this test in old PS2
+      current_powershell_major_version >= 3 ?
+        'Write-Output' :
+        'Write-Host'
+    end
 
+    it "should be able to write more than the 64k default buffer size to the managers pipe without deadlocking the Ruby parent process or breaking the pipe" do
       # this was tested successfully up to 5MB of text
       buffer_string_96k = 'a' * ((1024 * 96) + 1)
       result = manager.execute(<<-CODE
-'#{buffer_string_96k}' | Write-Output
+'#{buffer_string_96k}' | #{output_cmdlet}
         CODE
         )
 
       expect(result[:errormessage]).to eq(nil)
       expect(result[:exitcode]).to eq(0)
-      expect(result[:stdout]).to eq("#{buffer_string_96k}\r\n")
+      terminator = output_cmdlet == 'Write-Output' ? "\r\n" : "\n"
+      expect(result[:stdout]).to eq("#{buffer_string_96k}#{terminator}")
     end
 
     it "should be able to write more than the 64k default buffer size to child process stdout without deadlocking the Ruby parent process" do
       result = manager.execute(<<-CODE
 $bytes_in_k = (1024 * 64) + 1
-[Text.Encoding]::UTF8.GetString((New-Object Byte[] ($bytes_in_k))) | Write-Output
+[Text.Encoding]::UTF8.GetString((New-Object Byte[] ($bytes_in_k))) | #{output_cmdlet}
         CODE
         )
 
       expect(result[:errormessage]).to eq(nil)
       expect(result[:exitcode]).to eq(0)
-      expect(result[:stdout]).not_to eq(nil)
+      terminator = output_cmdlet == 'Write-Output' ? "\r\n" : "\n"
+      expected = "\x0" * (1024 * 64 + 1) + terminator
+      expect(result[:stdout]).to eq(expected)
     end
 
     it "should return a response with a timeout error if the execution timeout is exceeded" do
