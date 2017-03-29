34d35f756278f26783ad4c6557f106384d208d61
Glenn Sarti <glennsarti@users.noreply.github.com>
2017-03-28 17:26:36 -0700

    (MODULES-3945) Add acceptance tests for Non Windows platforms
    Previously the acceptance tests for the PowerShell module assumed that the
    systems under test were always on a Windows platform.  This commit extends the
    acceptance tests to Non-Windows platforms by:
    - Adding a provisioning step to install PowerShell depending on the platform
      type.  This is required as the tests assume PowerShell is already installed.
    - Attempting to test on an unsupported platform e.g. AIX will now error during
      provisioning instead of just failing tests
    - Modified Acceptance tests to flip which string to use based on the host
      platform e.g. Whether to use cmd.exe or sh -c
    - Added Non-Windows (POSIX) test fixtures for use in acceptance tests as the
      Windows fixtures are not valid in POSIX platforms e.g. Get-Service is not
      available on PowerShell POSIX yet
    - Moved the cleaning of the test files to the `before suite` as tests should
      ensure their state before they are tested instead of depending on a previous
      test run to clean up after itself

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -16,6 +16,38 @@ unless ENV['MODULE_provision'] == 'no'
   %w(lib metadata.json).each do |file|
     scp_to default, "#{proj_root}/#{file}", target
   end
+
+  # Install PowerShell on hosts that are not a Master, Dashboard or Database
+  agents.each do |host|
+    if not_controller(host)
+      case host.platform
+      when "ubuntu-14.04-amd64"
+        # Instructions for installing on Ubuntu 14 from
+        # https://github.com/PowerShell/PowerShell/blob/master/docs/installation/linux.md#ubuntu-1404
+        on(host,'curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -')
+        on(host,'curl https://packages.microsoft.com/config/ubuntu/14.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list')
+        on(host,'sudo apt-get update')
+        on(host,'sudo apt-get install -y powershell')
+      when "ubuntu-16.04-amd64"
+        # Instructions for installing on Ubuntu 16 from
+        # https://github.com/PowerShell/PowerShell/blob/master/docs/installation/linux.md#ubuntu-1604
+        on(host,'curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -')
+        on(host,'curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list')
+        on(host,'sudo apt-get update')
+        on(host,'sudo apt-get install -y powershell')
+      when "el-7-x86_64"
+        # Instructions for installing on CentOS 7, Oracle Linux7, RHEL 7 from
+        # https://github.com/PowerShell/PowerShell/blob/master/docs/installation/linux.md#centos-7
+        # sudo is not required and seems to throw errors when running under beaker `sudo: sorry, you must have a tty to run sudo`
+        on(host,'curl https://packages.microsoft.com/config/rhel/7/prod.repo | tee /etc/yum.repos.d/microsoft.repo')
+        on(host,'yum install -y powershell')
+      when /^windows/
+        # No need to do anything
+      else
+        raise("Unable to install PowerShell on host '#{host.name}' with platform '#{host.platform}'")
+      end
+    end
+  end
 end
 
 RSpec.configure do |c|
@@ -27,10 +59,17 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     shell("/bin/touch #{default['puppetpath']}/hiera.yaml")
-  end
-  c.after :suite do
+
+    powershell_agents = agents.select { |a| not_controller(a) }
+    posix_agents      = powershell_agents.select { |a| a.platform =~ /^(?!windows).*$/ }
+    windows_agents    = powershell_agents.select { |a| a.platform =~ /^windows.*$/ }
+
+    # Ensure test files don't exist before we run the test suite
     absent_files = 'file{["c:/services.txt","c:/process.txt","c:/try_success.txt","c:/catch_shouldntexist.txt","c:/try_shouldntexist.txt","c:/catch_success.txt"]: ensure => absent }'
-    apply_manifest(absent_files)
+    apply_manifest_on(windows_agents,absent_files) if windows_agents.count > 0
+
+    absent_files = 'file{["/services.txt","/process.txt","/try_success.txt","/catch_shouldntexist.txt","/try_shouldntexist.txt","/catch_success.txt"]: ensure => absent }'
+    apply_manifest_on(posix_agents,absent_files) if posix_agents.count > 0
   end
 end
 
