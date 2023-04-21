0d2c7d5ba8c62a16e8fc43e4ed0417b38407c8e8
jordanbreen28 <jordan.breen@perforce.com>
2023-04-21 14:23:17 +0100

    (CONT-793) - Correct Lint/DeprecatedClassMethods

--- spec/unit/provider/exec/powershell_spec.rb
+++ spec/unit/provider/exec/powershell_spec.rb
@@ -30,9 +30,9 @@ describe Puppet::Type.type(:exec).provider(:powershell) do
   end
 
   let(:powershell) {
-    if File.exists?("#{ENV['SYSTEMROOT']}\\sysnative\\WindowsPowershell\\v1.0\\powershell.exe")
+    if File.exist?("#{ENV['SYSTEMROOT']}\\sysnative\\WindowsPowershell\\v1.0\\powershell.exe")
       "#{ENV['SYSTEMROOT']}\\sysnative\\WindowsPowershell\\v1.0\\powershell.exe"
-    elsif File.exists?("#{ENV['SYSTEMROOT']}\\system32\\WindowsPowershell\\v1.0\\powershell.exe")
+    elsif File.exist?("#{ENV['SYSTEMROOT']}\\system32\\WindowsPowershell\\v1.0\\powershell.exe")
       "#{ENV['SYSTEMROOT']}\\system32\\WindowsPowershell\\v1.0\\powershell.exe"
     else
       'powershell.exe'
